9041f4f5aff7ee92db534dc756776cec441503e4
hetengjiao <hetengjiao@chinamobile.com>
2019-08-16 20:44:27 +0800

    spring boot

---
+++ service-manager/src/main/java/com/hetengjiao/demo/controller/ControllerBean.java
@@ -0,0 +1,83 @@
+package com.hetengjiao.demo.controller;
+
+import com.hetengjiao.demo.entities.BeanUser;
+import com.hetengjiao.demo.mapper.BeanUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+
+public class ControllerBean {
+
+	@Autowired
+	BeanUserMapper beanUserMapper;
+
+	//查询
+
+	@GetMapping("/get/{id}/user/{user_name}")
+
+	public List<BeanUser> getBeanUser(@PathVariable("id") Integer id,
+	                            @PathVariable("user_name") String user_name){
+		Map map = new HashMap();
+		map.put("id", id);
+		map.put("userName", user_name);
+		return beanUserMapper.selectId(map);
+
+	}
+	@GetMapping("/get-by-entity/{id}/user/{user_name}")
+
+	public List<BeanUser> getBeanUser1(@PathVariable("id") Integer id,
+	                                  @PathVariable("user_name") String user_name){
+		BeanUser beanUser = new BeanUser();
+		beanUser.setId(id);
+		beanUser.setUserName(user_name);
+		return beanUserMapper.selectByEntity(beanUser);
+
+	}
+	//插入
+
+	@PostMapping(value = "/insert")
+
+	public BeanUser insertBeanUser(@RequestBody BeanUser beanUser){
+
+		beanUserMapper.insertBean(beanUser);
+
+		return beanUser;
+
+	}
+
+	@PostMapping(value = "/insert_batch")
+
+	public List<BeanUser> insertBeanUserBatch(@RequestBody List<BeanUser> beanUser){
+
+		beanUserMapper.insertBeanBatch(beanUser);
+
+		return beanUser;
+
+	}
+	//修改
+
+	@PostMapping("/update")
+
+	public BeanUser updateBeanUser(@RequestBody BeanUser beanUser){
+
+		beanUserMapper.UpdateBean(beanUser);
+
+		return beanUser;
+
+	}
+	//删除
+
+	@DeleteMapping("/delete/{id}")
+	public String deleteBeanUser(@PathVariable("id")Integer id){
+
+		beanUserMapper.deleteId(id);
+
+		return "删除成功！";
+
+	}
+}
---
+++ service-manager/src/main/java/com/hetengjiao/demo/mapper/BeanUserMapper.java
@@ -0,0 +1,36 @@
+package com.hetengjiao.demo.mapper;
+
+//指定这是一个操作数据库的mapper
+
+import com.hetengjiao.demo.entities.BeanUser;
+import org.apache.ibatis.annotations.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface BeanUserMapper {
+
+
+
+	//查询,map方式
+	List<BeanUser> selectId(Map param);
+	//查询,entity方式
+	List<BeanUser> selectByEntity(BeanUser beanUser);
+
+	//单条插入
+	int insertBean(BeanUser beanUser);
+
+	//批量插入
+	int insertBeanBatch(List<BeanUser> beanUserList);
+
+	//修改
+
+	//@Update("update table_one set user_name=#{user_name},pass_word=#{pass_word} where id=#{id}")
+
+	int UpdateBean(BeanUser beanUser);
+	int deleteId(Integer id);
+
+
+}
