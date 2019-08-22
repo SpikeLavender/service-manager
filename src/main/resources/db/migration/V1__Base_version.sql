b67d2ddcd963636bc0690de583ba4e8d9af33856
hetengjiao <hetengjiao@chinamobile.com>
2019-08-22 18:30:47 +0800

    modify createtime

--- src/main/resources/db/migration/V1__Base_version.sql
+++ src/main/resources/db/migration/V1__Base_version.sql
@@ -5,7 +5,7 @@ CREATE TABLE `user_info`  (
                               `user_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
                               `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
                               `token` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-                              `create_time` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+                              `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
                               `update_time` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                               `expire_time` datetime(0) NULL DEFAULT NULL,
                               PRIMARY KEY (`id`) USING BTREE,
@@ -24,7 +24,7 @@ CREATE TABLE `order_info`  (
                                `app_list` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
                                `fee` double(255, 0) NULL DEFAULT NULL,
                                `order_status` tinyint(255) NULL DEFAULT NULL,
-                               `create_time` datetime(0) NULL DEFAULT NULL,
+                               `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
                                `update_time` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                                PRIMARY KEY (`id`) USING BTREE,
                                INDEX `user_id_index`(`user_id`) USING BTREE
